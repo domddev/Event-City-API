2357cf0747a60cf3a5e5562e583854135b3adad6
domddev <domdstudios56@gmail.com>
2024-10-12 12:45:21 +0200

    Update GamemodesAPI.lua

--- GamemodesAPI.lua
+++ GamemodesAPI.lua
@@ -2,3 +2,11 @@ local activated = true
 
 print("API Successfully connected.")
 print(activated)
+
+game.Players.PlayerAdded:Connect(function(plr)
+	local screenGui = Instance.new("ScreenGui")
+	screenGui.Parent = plr.PlayerGui
+	
+	local frame = Instance.new("Frame")
+	frame.Parent = screenGui
+end)
