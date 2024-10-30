337325b172229884b82c1ca8d8e9815a8ba4b0b9
domddev <domdstudios56@gmail.com>
2024-10-30 11:34:05 +0100

    Update Downtime.lua

--- Downtime.lua
+++ Downtime.lua
@@ -1,9 +1,15 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Stages = ReplicatedStorage.Stages
+	local Stages = ReplicatedStorage.Stages
 
-print("Sent Code to game")
-Stages.Animation:FindFirstChild("Character_Stage").Value = "Stage1"
-Stages.Countdown:FindFirstChild("Countdown_Enabled").Value = false
-Stages.Countdown:FindFirstChild("TV_Volume").Value = 0.06
+	print("Sent Code to game")
+	Stages.Animation:FindFirstChild("Character_Stage").Value = "Stage2"
+	
+	Stages.Countdown:FindFirstChild("Countdown_Enabled").Value = false
+	Stages.Countdown:FindFirstChild("TV_Volume").Value = 0.06
+	
+	Stages.BlackHole:FindFirstChild("ToAdd").Value = "BlackholeStage1"
+	Stages.BlackHole:FindFirstChild("ToRemove").Value = "BlackholeStage1"
+	
+	Stages.SendUpdate:FireAllClients()
 
-Stages.SendUpdate:FireAllClients()
+	task.wait(waitingTime)
