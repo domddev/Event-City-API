305f8aa228bcd0360e4b0dda731f27d5d479aaa1
domddev <domdstudios56@gmail.com>
2024-10-30 10:46:01 +0100

    Delete GamemodesAPI.lua

--- GamemodesAPI.lua
+++
@@ -1,5 +0,0 @@
-local gamemodeRERequest = game.ReplicatedStorage.GamemodeRE.sendRequestGamemode
-
-gamemodeRERequest:FireAllClients("Explore", true)
-gamemodeRERequest:FireAllClients("FFA", false)
-gamemodeRERequest:FireAllClients("TheFinale", true)
