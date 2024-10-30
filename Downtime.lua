ccc56c79415132dd734074626db67a1285afff9b
domddev <domdstudios56@gmail.com>
2024-10-30 11:01:02 +0100

    Update Downtime.lua

--- Downtime.lua
+++ Downtime.lua
@@ -4,5 +4,6 @@ local Stages = ReplicatedStorage.Stages
 print("Sent Code to game")
 Stages.Animation:FindFirstChild("Character_Stage").Value = "Stage1"
 Stages.Countdown:FindFirstChild("Countdown_Enabled").Value = false
+Stages.Countdown:FindFirstChild("TV_Volume").Value = 0.06
 
 Stages.SendUpdate:FireAllClients()
