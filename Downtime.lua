9d7d665023f78082e857e00f5f18e817bab1f4c4
domddev <domdstudios56@gmail.com>
2024-10-30 12:55:44 +0100

    Update Downtime.lua

--- Downtime.lua
+++ Downtime.lua
@@ -10,7 +10,7 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 	Stages.BlackHole:FindFirstChild("ToAdd").Value = "BlackholeStage1"
 	Stages.BlackHole:FindFirstChild("ToRemove").Value = "BlackholeStage1"
 
-	Stages.Countdown:FindFirstChild("TV_Stage").Value = "Stage1"
+	Stages.Countdown:FindFirstChild("TV_Stage").Value = "Stage2"
 
 	Stages:FindFirstChild("SendUpdate"):FireAllClients()
 
