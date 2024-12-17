868bd1b2ef6a871c20e5b266d7027100c7008b2b
domddev <domdstudios56@gmail.com>
2024-12-17 20:48:07 +0100

    Update DowntimeAPI.lua

--- DowntimeAPI.lua
+++ DowntimeAPI.lua
@@ -5,8 +5,8 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 	Stages.Animation:FindFirstChild("Character_Stage").Value = "Stage5"
 	
 	Stages.Countdown:FindFirstChild("Countdown_Enabled").Value = false
-	Stages.Countdown:FindFirstChild("TV_Volume").Value = 2
-	Stages.Countdown:FindFirstChild("TV_Stage").Value = "Stage3"
+	Stages.Countdown:FindFirstChild("TV_Volume").Value = 2.4
+	Stages.Countdown:FindFirstChild("TV_Stage").Value = "Stage4"
 
 	game.Workspace.NightNightBH:ClearAllChildren()
 	
